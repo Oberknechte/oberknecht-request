0641db0e3510ded876ca28e51421605097dd50fc
jubewe <ju.be.we@posteo.de>
2023-11-19 14:14:17 +0100

    v0.1.0

--- lib-js/types/request.d.ts
+++ lib-js/types/request.d.ts
@@ -1,16 +1,16 @@
 import { CoreOptions, RequestResponse } from "request";
 export declare const requestMethods: readonly ["CONNECT", "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT", "TRACE"];
-export type requestMethodType = typeof requestMethods[number];
+export declare type requestMethodType = typeof requestMethods[number];
 export declare const defaultHeaders: readonly ["Authorization", "Auth", "Accept", "Accept-Encoding", "Accept-Language", "Content-Type", "Content-Encoding", "Content-Language"];
-export type defaultHeaderType = Record<typeof defaultHeaders[number], string | any>;
+export declare type defaultHeaderType = Record<typeof defaultHeaders[number], string | any>;
 export declare const requestCallback: (e: Error, r: RequestResponse, f: RequestResponse | Error) => void;
-export type requestOptions = CoreOptions & {
+export declare type requestOptions = CoreOptions & {
     method?: requestMethodType;
     headers?: defaultHeaderType;
     body?: string;
     json?: boolean;
 };
-export type globalOptions = {
+export declare type globalOptions = {
     callbackOptions?: {
         callback: Function;
     };
