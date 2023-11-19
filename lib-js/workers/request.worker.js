f7609311bedf6a506d6e64eaa071be67ba3940ae
jubewe <ju.be.we@posteo.de>
2023-11-19 15:03:21 +0100

    v0.1.1

--- lib-js/workers/request.worker.js
+++ lib-js/workers/request.worker.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const worker_threads_1 = require("worker_threads");
 const request_1 = __importDefault(require("request"));
 const { url, options } = worker_threads_1.workerData;
-(0, request_1.default)(url, options, (e, r) => {
-    worker_threads_1.parentPort.postMessage(JSON.stringify({ "e": e, "r": r }));
-});
+try {
+    (0, request_1.default)(url, options, (e, r) => {
+        worker_threads_1.parentPort.postMessage(JSON.stringify({ e: e, r: r }));
+    });
+}
+catch (e) {
+    worker_threads_1.parentPort.postMessage(JSON.stringify({ e: Error("Request failed", { cause: e }), r: undefined }));
+}
--- lib-ts/workers/request.worker.ts
+++ lib-ts/workers/request.worker.ts
@@ -2,6 +2,12 @@ import { parentPort, workerData } from "worker_threads";
 import request from "request";
 
 const { url, options } = workerData;
-request(url, options, (e, r) => {
-    parentPort.postMessage(JSON.stringify({ "e": e, "r": r }));
-});
+try {
+  request(url, options, (e, r) => {
+    parentPort.postMessage(JSON.stringify({ e: e, r: r }));
+  });
+} catch (e) {
+  parentPort.postMessage(
+    JSON.stringify({ e: Error("Request failed", { cause: e }), r: undefined })
+  );
+}
